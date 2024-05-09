3f8db5ff6dc7b85111feaa9c768bac3da4328d4c
jamernam121 <121072608+jamernam121@users.noreply.github.com>
2024-05-09 15:10:28 +0700

    Update w7x64.sh

--- .github/workflows/w7x64.sh
+++ .github/workflows/w7x64.sh
@@ -11,7 +11,7 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86_64 -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda w7x64.img -m 2G -smp cores=2 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=2 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
@@ -26,4 +26,3 @@ echo "Wait 2 minute to finish bot"
 echo "You can close this tab"
 echo "RDP runs for 50 hours"
 echo "===================================="
-sleep 432000
