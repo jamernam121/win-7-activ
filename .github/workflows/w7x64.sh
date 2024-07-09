e92d6df2d769fceafb130313ff81d5f8307b003b
jamernam121 <121072608+jamernam121@users.noreply.github.com>
2024-07-09 18:56:48 +0700

    Update w7x64.sh

--- .github/workflows/w7x64.sh
+++ .github/workflows/w7x64.sh
@@ -1,28 +1,22 @@
-apt-get update
-echo "Download windows files"
+echo "Đang tải file xuống - Download windows files"
 wget -O w7x64.img https://bit.ly/akuhnetw7X64
 echo "Download ngrok"
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-unzip ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
-echo Downloading File From akuh.net
-apt-get install qemu-system-x86_64 -y
+nohup ./ngrok tcp 3388 &>/dev/null &
+echo Downloading File From toigamo.blogspot.com
+apt install qemu-kvm -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=2 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
-clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "Username: akuh"
-echo "Password: Akuh.Net"
-echo "===================================="
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free RDP now"
-echo "Wait 2 minute to finish bot"
-echo "You can close this tab"
-echo "RDP runs for 50 hours"
-echo "===================================="
+echo "Ctrl+C để copy - Ctrl+C To Copy"
+echo Username: akuh
+echo Password: Akuh.Net
+echo "Chờ 1-2 phút để hoàn tất - Wait 1-2 minute to finish bot"
+echo "Vui lòng không tắt cửa sổ này - Dont Close This Tab"
+echo "Please support toigamo.blogspot.com thank you"
+qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+sleep 43200
