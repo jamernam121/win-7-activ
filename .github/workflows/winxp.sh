790769770754d0a2164d53fa8f68b276ae61c4dd
jamernam121 <121072608+jamernam121@users.noreply.github.com>
2024-09-02 09:01:41 +0700

    Update winxp.sh

--- .github/workflows/winxp.sh
+++ .github/workflows/winxp.sh
@@ -1,10 +1,12 @@
 sudo apt update -y
 sudo apt install wget curl -y
-wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz &>/dev/null &
- tar -zxvf ngrok.tgz
-./ngrok config add-authtoken "2coWZi6lp6Yvn7QmBhG8Z7Bqc32_2NMWmXTdrGrUxPncJF8sr"
+sudo apt install qemu-kvm
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -zxvf ngrok.tgz
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP
 ./ngrok http 8006 --region ap &>/dev/null &
- sudo chmod 666 /dev/kvm
+sudo chmod 666 /dev/kvm
 sudo mkdir /isos/
 sudo wget -O /isos/a.iso "https://computernewb.com/isos/windows/Win10_22H2_English_x64.iso" &>/dev/null &
  while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
