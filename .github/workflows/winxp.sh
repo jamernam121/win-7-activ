8c4608c8fa318e56788fef97abb68d513f510597
jamernam121 <121072608+jamernam121@users.noreply.github.com>
2024-09-02 08:58:14 +0700

    Update winxp.sh

--- .github/workflows/winxp.sh
+++ .github/workflows/winxp.sh
@@ -1,12 +1,15 @@
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-unzip hngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
-echo Please wait for installing...
-sudo apt update -y > /dev/null 2>&1
-echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
-echo Downloading Windows Disk...
-curl -L -o litexp.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.0/providers/qemu.box
-sudo qemu-system-x86_64 -vnc :0 -hda litexp.qcow2  -smp cores=4 -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo apt update -y
+sudo apt install wget curl -y
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz &>/dev/null &
+ tar -zxvf ngrok.tgz
+./ngrok config add-authtoken "2coWZi6lp6Yvn7QmBhG8Z7Bqc32_2NMWmXTdrGrUxPncJF8sr"
+./ngrok http 8006 --region ap &>/dev/null &
+ sudo chmod 666 /dev/kvm
+sudo mkdir /isos/
+sudo wget -O /isos/a.iso "https://computernewb.com/isos/windows/Win10_22H2_English_x64.iso" &>/dev/null &
+ while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
+ sudo wget -O /isos/driver.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.240-1/virtio-win-0.1.240.iso &>/dev/null &
+while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
+ wget https://raw.githubusercontent.com/jamernam121/t300k/main/compose.yaml
+echo "thanh cong, ket noi bang port 8006 o trinh duyet Chrome"
+sudo docker compose up
