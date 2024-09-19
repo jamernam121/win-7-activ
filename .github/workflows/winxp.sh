682a04875de27170da61ab9b7f2b3a8c24df856e
jamernam121 <121072608+jamernam121@users.noreply.github.com>
2024-09-19 20:53:44 +0700

    Update winxp.sh

--- .github/workflows/winxp.sh
+++ .github/workflows/winxp.sh
@@ -3,9 +3,9 @@ sudo apt install wget curl -y
 sudo apt install qemu-kvm
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -zxvf ngrok.tgz
-./ngrok authtoken 2coWZi6lp6Yvn7QmBhG8Z7Bqc32_2NMWmXTdrGrUxPncJF8sr
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP
 ./ngrok http 8006 --region ap &>/dev/null &
-sudo chmod 666 /dev/kvm
 sudo mkdir /isos/
 sudo wget -O /isos/a.iso "https://computernewb.com/isos/windows/Win10_22H2_English_x64.iso" &>/dev/null &
  while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
@@ -13,4 +13,5 @@ sudo wget -O /isos/a.iso "https://computernewb.com/isos/windows/Win10_22H2_Engli
 while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
  wget https://raw.githubusercontent.com/jamernam121/t300k/main/compose.yaml
 echo "thanh cong, ket noi bang port 8006 o trinh duyet Chrome"
+sudo apt-get install docker-compose-plugin
 sudo docker compose up
